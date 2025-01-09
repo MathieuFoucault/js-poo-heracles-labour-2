a5ed9d43ffd4ad7e2c25d3d2c21dee29e2d638dd
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2025-01-09 12:16:03 +0100

    feat : heracles labour 2 resolved

--- src/Fighter.js
+++ src/Fighter.js
@@ -1,34 +1,41 @@
 const MAX_LIFE = 100;
 
 class Fighter {
-    constructor(name, strength, dexterity) {
-        this.name = name;
-        this.strength = strength;
-        this.dexterity = dexterity;
-        this.life = MAX_LIFE;
-    }
+	constructor(name, strength, dexterity) {
+		this.name = name;
+		this.strength = strength;
+		this.dexterity = dexterity;
+		this.life = MAX_LIFE;
+		this.weapon = null;
+		this.shield = null;
+	}
 
+	getDamage() {
+		return this.strength + (this.weapon ? this.weapon.damage : 0);
+	}
 
-    // Launch a fight
-    fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
+	getDefense() {
+		return this.dexterity + (this.shield ? this.shield.protection : 0);
+	}
 
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
+	// Launch a fight
+	fight(defender) {
+		const attackPoints = this.getRandomInt(this.getDamage());
 
-        defender.life = Math.max(defender.life - damages, 0);
-    }
+		const damages = Math.max(attackPoints - defender.getDefense(), 0);
 
+		defender.life = Math.max(defender.life - damages, 0);
+	}
 
-    // Generate a random value between 1 and max
-    getRandomInt(max) {
-        return 1 + Math.floor(Math.random() * max);
-    }
+	// Generate a random value between 1 and max
+	getRandomInt(max) {
+		return 1 + Math.floor(Math.random() * max);
+	}
 
-
-    // Determine if a fighter is still alive
-    isAlive() {
-        return this.life > 0;
-    }
+	// Determine if a fighter is still alive
+	isAlive() {
+		return this.life > 0;
+	}
 }
 
 module.exports = Fighter;
